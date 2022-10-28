d8f1c44f91dbaf17dd30562857ac9e8ee836295b
tonlirise <anton.kladko@touchin.ru>
2022-10-29 03:51:10 +0700

    Add main activity and fragments

---
+++ app/src/main/java/com/example/randomnumberapp/main/presentation/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.randomnumberapp.main.presentation
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.randomnumberapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ app/src/main/java/com/example/randomnumberapp/numbers/presentation/NumbersFragment.kt
@@ -0,0 +1,14 @@
+package com.example.randomnumberapp.numbers.presentation
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.randomnumberapp.R
+
+class NumbersFragment : Fragment() {
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        return inflater.inflate(R.layout.fragment_numbers, container, false)
+    }
+}
